e8cf2df66a467c4ce89c5cd66177c3afaff19afb
yxstagrabben <67810043+yxstagrabben@users.noreply.github.com>
2021-10-07 12:56:11 +0200

    Add files via upload

--- JS/JavaScript/myScript.js
+++ JS/JavaScript/myScript.js
@@ -1,11 +1,22 @@
+const text = ["YEEEEEE BOOOOOY", "WEE", "LOOOOVVEEEEEE", "WEBBBB", "SERVER", "PROGRAMMING", "AND", "ARNAUD"];
+
+var amount = -1;
+
 function changeTitle(){
-    document.getElementById("title").innerHTML = "YEEEEE BOOOOY"
+    if(amount === 7)
+    {
+        amount = -1;
+    }
+    amount = amount + 1;
+    document.getElementById("title").innerHTML = text[amount];
+    console.log(amount);
 }
 function goToNewWebsite(){
     window.location='https://www.youtube.com/watch?v=dQw4w9WgXcQ';
 }
-function json(){
-    const obj = JSON.parse('{"name":"Anton", "age":"16", "car":"Nissan Skyline GTR R34", "city":"Örebro"}')
+
+window.onload = function(){
+    const obj = JSON.parse('{"name":"Anton","age":"16","car":"Nissan Skyline GTR R34","city":"Örebro"}');
     document.getElementById("jsonTextName").innerHTML = obj.name;
     document.getElementById("jsonTextAge").innerHTML = obj.age;
     document.getElementById("jsonTextCar").innerHTML = obj.car;
